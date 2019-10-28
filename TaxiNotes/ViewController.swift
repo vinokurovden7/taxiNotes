69539729922743afc2cdfba3ccacd5c04eb6dd89
Denis <vinokurovden7@gmail.com>
2019-10-28 15:39:36 +0500

    Initial Commit

---
+++ TaxiNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaxiNotes
+//
+//  Created by Денис Винокуров on 28.10.2019.
+//  Copyright © 2019 Денис Винокуров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TaxiNotesTests/TaxiNotesTests.swift
@@ -0,0 +1,34 @@
+//
+//  TaxiNotesTests.swift
+//  TaxiNotesTests
+//
+//  Created by Денис Винокуров on 28.10.2019.
+//  Copyright © 2019 Денис Винокуров. All rights reserved.
+//
+
+import XCTest
+@testable import TaxiNotes
+
+class TaxiNotesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
